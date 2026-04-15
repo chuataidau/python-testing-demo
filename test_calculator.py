3e5ba247a8371629dbcfa8fcfe8e163cc59a68b1
chuataidau1 <nhhien1403@gmail.com>
2026-04-15 16:53:36 +0700

    Add tests for path coverage, closes #2

--- test_calculator.py
+++ test_calculator.py
@@ -6,5 +6,11 @@ class TestCalculator(unittest.TestCase):
     def test_basic(self):
         self.assertEqual(sum_even_numbers(5), 6)
 
+    def test_zero(self):
+        self.assertEqual(sum_even_numbers(0), 0)
+
+    def test_no_even(self):
+        self.assertEqual(sum_even_numbers(1), 0)
+
 if __name__ == "__main__":
     unittest.main()
